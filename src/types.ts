25d6f60b2f272b88e1eb27094d9bf0dd4f715244
Daniel M. Bomfim <daniellsmv@hotmail.com>
2023-10-31 22:18:50 -0300

    docs(calendar): Adds TypeDocs for the types exposed by the Calendar components

--- src/types.ts
+++ src/types.ts
@@ -19,23 +19,49 @@ export interface CalendarContext {
 }
 
 export interface CalendarContainerProps {
+	/** A node of components to be rendered inside the calendar */
 	children: ReactNode;
+	/**
+	 * A callback to be called every time the selected date changes
+	 * @param selectedDate the new date selected
+	 * @returns void
+	 */
 	onSelectionChanged?: (selectedDate: Date | null) => void;
 }
 
 export interface CalendarHeaderProps {
+	/** A string to be rendered on the header */
 	text?: string;
 }
 
 export interface CalendarColumnProps {
+	/**
+	 * The index of the week day the column correspond to.
+	 * Start at '0' for sunday and ends at '6' for sunday
+	 */
 	weekDay: CalendarLabelsIndex;
+	/**
+	 * A function that renders the label of a column
+	 * @param weekDay The index corresponding to the column
+	 * @returns A string or an element to be rendered
+	 */
 	renderText?: (weekDay: CalendarLabelsIndex) => string | ReactElement;
 }
 
 export interface CalendarItemsContainerProps {
+	/** The start of the range of dates to be displayed */
 	startDate: Date;
+	/** The end of the range of dates to be displayed */
 	endDate: Date;
+	/** Defines if the calendar will render dates outside of the defined
+	 * range to fill all empty slots on the rows */
 	fillEmptySlots?: boolean;
+	/**
+	 * A function responsible for rendering the items within the calendar
+	 * @param itemData The props of the calendar item element
+	 * @param key A unique key for the element
+	 * @returns The react element to be displayed on the calendar
+	 */
 	renderItem?: (
 		itemData: CalendarItemProps,
 		key: number | string
@@ -43,9 +69,17 @@ export interface CalendarItemsContainerProps {
 }
 
 export interface CalendarItemProps {
+	/** The date of the item */
 	itemDate: Date | null;
+	/** Defines if the user can interact with the item */
 	disabled: boolean;
+	/** The status of the item. This change the style according the status */
 	status: 'normal' | 'pending' | 'alert' | 'done' | 'hidden';
+	/**
+	 * A function responsible for rendering the item text
+	 * @param date The date of the item
+	 * @returns A string or Element to be rendered on the item
+	 */
 	renderText: (date: Date) => string | ReactElement;
 }
 
