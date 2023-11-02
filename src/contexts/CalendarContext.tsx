31c6ba8bc4bd11c230f6aa7ff67cd6cb0850de9c
Daniel M. Bomfim <daniellsmv@hotmail.com>
2023-11-02 14:55:22 -0300

    perf(CalendarContext): wraps the value returned by the context in a useMemo hook to avoid unnecessary rendering

--- src/contexts/CalendarContext.tsx
+++ src/contexts/CalendarContext.tsx
@@ -1,4 +1,10 @@
-import { createContext, useState, useContext, PropsWithChildren } from 'react';
+import {
+	createContext,
+	useState,
+	useContext,
+	PropsWithChildren,
+	useMemo
+} from 'react';
 import { CalendarContext, CalendarLabelsIndice } from '../types';
 
 const calendarContext = createContext<CalendarContext>({} as CalendarContext);
@@ -9,19 +15,22 @@ export const CalendarProvider = ({ children }: PropsWithChildren) => {
 	const [columnsNumber, setColumnsNumber] = useState<number>(0);
 	const [selectedItem, setSelectedItem] = useState<number | null>(null);
 
+	const contextValue = useMemo(
+		() => ({
+			columnsNumber,
+			setColumnsNumber,
+			selectedItem,
+			setSelectedItem,
+			dates,
+			setDates,
+			columns,
+			setColumns
+		}),
+		[columnsNumber, selectedItem, dates, columns]
+	);
+
 	return (
-		<calendarContext.Provider
-			value={{
-				columnsNumber,
-				setColumnsNumber,
-				selectedItem,
-				setSelectedItem,
-				dates,
-				setDates,
-				columns,
-				setColumns
-			}}
-		>
+		<calendarContext.Provider value={contextValue}>
 			{children}
 		</calendarContext.Provider>
 	);
